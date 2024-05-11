1362ccca106e176b5763d7576f6b72f22d393e89
AnnabellexSmith <sannabelle056@gmail.com>
2024-05-11 12:00:06 -0700

    CRUD fucntions for drugs working

--- controllers/drugs.js
+++ controllers/drugs.js
@@ -2,14 +2,66 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 const Indications = require('../models/indication.js');
+const Drug = require('../models/drug.js');
+const drug = require('../models/drug.js');
+
+//Index
+router.get('/index', async (req, res) => {
+    const userDrugs = await Drug.find({ owner: req.session.user._id})
+    res.render('drugs/index', { drugs: userDrugs});
+});
+
+//Create
+router.get('/new', async (req, res) => {
+    res.render('drugs/new');
+});
+
+//POST
+router.post('/', async (req, res)=> {
+    if (req.body.reactive) {
+        req.body.reactive = true;
+    } else {
+        req.body.reactive = false;
+    }
+    req.body.owner = req.session.user._id
+    await Drug.create(req.body);
+    res.redirect('drugs/index');
+})
+
+// SHOW- individual medication based on id
+router.get('/:drugId', async (req, res) => {
+    const drugId = await Drug.findById(req.params.drugId);
+    // let reactiveValue = (!drugId.reactive) ? 'off' : 'on'
+    res.render('drugs/show', { drug: drugId });
+})
 
 
+//Edit - individual medicaiton based on id
+router.get('/:drugId/edit', async (req, res) => {
+    const drugId = await Drug.findById(req.params.drugId);
+    res.render('drugs/edit', { drug: drugId});
+})
 
+///Update - individual medication based on id
+router.put('/:drugId', async (req, res) => {
+    if (req.body.reactive) {
+            req.body.reactive= true;
+        } else {
+        req.body.reactive= false;
+    }
+    req.body.owner = req.session.user._id
+    const updatedDrug = await drug.findByIdAndUpdate(
+        req.params.drugId,
+        req.body,
+        { new : true }
+    )
+    res.render('drugs/show', { drug : updatedDrug })
+})
 
-
-
-
-
-
+//DELETE - individual medication based on id
+router.delete('/:drugId', async (req, res) => {
+    await Drug.findByIdAndDelete(req.params.drugId)
+    res.redirect('/drugs/index')
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const dotenv = require('dotenv').config();
-const session = require('express-session')
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
-const path = require('path');
+const session = require('express-session');
 
 
 const app = express();
-
 app.set('view engine', 'ejs');
+
+const path = require('path');
+
 
 mongoose.connect(process.env.MONGODBURI);
 mongoose.connection.on("connected", () => {
@@ -20,30 +21,31 @@ const authController = require('./controllers/auth.js');
 const drugsController = require('./controllers/drugs.js');
 const indicationController = require('./controllers/indications.js');
 
-
 const isSignedIn = require('./middleware/is-signed-in');
 const passUserToView = require('./middleware/pass-user-to-view');
-const user = require('./models/user');
+
 
 app.use(morgan('dev'));
 app.use(express.urlencoded ({ extended: false}));
 app.use(methodOverride('_method'));
-app.use(express.static('public'))
-app.use(express.static(path.join(__dirname, "public")));
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
   }));
-app.use(passUserToView);
-app.use('/auth', authController);
-app.use('/drugs', drugsController);
-app.use('/indications', indicationController);
-
+  app.use(passUserToView);
+app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => {
     res.render("index", {user: req.session.user})
 })
+
+app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/drugs', drugsController);
+app.use('/indications', indicationController);
+
 
 
 
